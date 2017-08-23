4c8c5fc62041480328e24f42f5c9e0d41d0d6d04
Jon Raymer <jon.raymer@lssinc.com>
2017-08-22 20:48:16 -0500

    resource owner password credential login flow

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -3,5 +3,6 @@ export const environment = {
   API_URL: 'http://qa-api-ms.voyageframework.com/api/v1',
   SERVER_URL: 'http://qa-web-ms.voyageframework.com',
   OAUTH_REDIRECT_URL: 'http://qa-app-ms.voyageframework.com',
-  OAUTH_CLIENT_ID: 'client-super-qa'
+  OAUTH_CLIENT_ID: 'client-super-qa',
+  OAUTH_CLIENT_SECRET: 'secret'
 };
