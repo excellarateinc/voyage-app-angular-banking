009289988896da1d2a68c1e41dc8da3b8324dd34
Jon Raymer <jon.raymer@lssinc.com>
2017-09-06 14:12:23 -0500

    chat updates

---
+++ src/app/chat/chat-message.model.ts
@@ -0,0 +1,8 @@
+export class ChatMessage {
+  messageId: number;
+  channelId: number;
+  message: string;
+  username: string;
+  createdBy: string;
+  createDate: Date;
+}
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from './chat.service';
+import { ChatChannel } from './chat-channel.model';
+import { ChatMessage } from './chat-message.model';
+import { SignalR } from 'ng2-signalr';
 
 @Component({
   selector: 'app-chat',
@@ -7,10 +10,61 @@ import { ChatService } from './chat.service';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  channels: Array<ChatChannel>;
+  currentChannel: ChatChannel;
+  newMessage: string;
+  private readonly chatMessage = 'newChatMessage';
 
-  constructor(private chatService: ChatService) { }
+  constructor(private chatService: ChatService, private signalR: SignalR) { }
 
   ngOnInit() {
+    this.chatService.getChannels()
+      .subscribe(result => this.channels = result);
+
+    this.listenForPushNotifications();
+  }
+
+  createChannel(): void {
+    this.chatService.createChannel()
+      .subscribe(result => {
+        this.currentChannel = result;
+        this.channels.push(result);
+      });
+  }
+
+  selectChannel(channel: ChatChannel): void {
+    this.currentChannel = channel;
+    this.chatService.getMessages(channel.channelId)
+      .subscribe(result => this.currentChannel.messages = result);
+  }
+
+  createMessage(message): void {
+    if (message == null || message === '') {
+      return;
+    }
+    this.chatService.createMessage(this.currentChannel.channelId, message)
+      .subscribe(result => this.currentChannel.messages.push(result));
+  }
+
+  private listenForPushNotifications(): void {
+    this.signalR.connect().then(connection => {
+      connection.listenFor(this.chatMessage)
+        .subscribe((message: ChatMessage) => {
+          this.onPushReceived(message);
+      });
+    });
+  }
+
+  private onPushReceived(message: ChatMessage): void {
+    const channels = this.channels.filter(channel => channel.channelId === message.channelId);
+
+    if (channels.length) {
+      const channel = channels[0];
+      if (!channel.messages) {
+        channel.messages = [];
+      }
+      channel.messages.push(message);
+    }
   }
 
 }
