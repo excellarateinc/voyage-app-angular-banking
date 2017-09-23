3ab95f54942d0da1470de02a43d75f76e608b56d
Jon Raymer <jon.raymer@lssinc.com>
2017-09-23 08:03:49 -0500

    Tweak to profile image save.

--- src/app/examples/profile/profile-image/profile-image.component.ts
+++ src/app/examples/profile/profile-image/profile-image.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, Input, Output, EventEmitter } from '@angular/core';
 import { ImageCropperComponent, CropperSettings, Bounds } from 'ng2-img-cropper';
+import { BroadcastService } from '../../../core/broadcast.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-profile-image',
   templateUrl: './profile-image.component.html',
   styleUrls: ['./profile-image.component.scss']
 })
-export class ProfileImageComponent implements OnInit {
+export class ProfileImageComponent implements OnInit, OnDestroy {
   @Input() currentImage: any;
   @Output() imageChanged = new EventEmitter<any>();
   @ViewChild('uploaderInput') uploaderInput: any;
   @ViewChild('cropper') cropper: ImageCropperComponent;
   cropperSettings: CropperSettings;
   data: any;
+  private broadcastWatcher: Subscription;
 
-  constructor() { }
+  constructor(private broadcastService: BroadcastService) { }
 
   ngOnInit() {
     this.initializeCropper();
     this.data = { image: this.currentImage };
+    this.broadcastWatcher = this.broadcastService.profileUpdated$
+      .subscribe(user => {
+        this.currentImage = user.profileImage;
+        this.data.image = null;
+      });
+  }
+
+  ngOnDestroy() {
+    this.broadcastWatcher.unsubscribe();
   }
 
   initFileUpload(): void {
